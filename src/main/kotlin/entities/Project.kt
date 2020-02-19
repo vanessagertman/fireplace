4144a786caa659f986def55447f2704c036736c0
vanessa gertman <vanessagertman@gmail.com>
2020-02-18 22:43:23 -0700

    Upgate Alpas to 0.15.0

--- src/main/kotlin/entities/Project.kt
+++ src/main/kotlin/entities/Project.kt
@@ -1,16 +1,12 @@
 
 package dev.alpas.fireplace.entities
 
-import dev.alpas.ozone.MigratingTable
-import dev.alpas.ozone.bigIncrements
-import dev.alpas.ozone.mediumText
-import me.liuwj.ktorm.entity.Entity
+import dev.alpas.ozone.*
 import me.liuwj.ktorm.schema.long
 import me.liuwj.ktorm.schema.text
-import me.liuwj.ktorm.schema.timestamp
 import java.time.Instant
 
-interface Project : Entity<Project> {
+interface Project : OzoneEntity<Project> {
     var id: Long
     val title: String
     val description: String
@@ -19,16 +15,16 @@ interface Project : Entity<Project> {
     var createdAt: Instant?
     var updatedAt: Instant?
 
-    companion object : Entity.Factory<Project>()
+    companion object : OzoneEntity.Of<Project>()
 }
 
 
-object Projects : MigratingTable<Project>("projects") {
-    val id by bigIncrements("id").bindTo { it.id }
+object Projects : OzoneTable<Project>("projects") {
+    val id by bigIncrements()
     val title by mediumText("title").bindTo { it.title }
     val description by text("description").bindTo { it.description }
     val notes by text("notes").nullable().bindTo { it.notes }
     val ownerId by long("owner_id").belongsTo(Users) { it.owner }
-    val createdAt by timestamp("created_at").useCurrent().bindTo { it.createdAt }
-    val updatedAt by timestamp("updated_at").useCurrent().bindTo { it.updatedAt }
+    val createdAt by createdAt()
+    val updatedAt by updatedAt()
 }
--- src/main/kotlin/entities/User.kt
+++ src/main/kotlin/entities/User.kt
@@ -3,18 +3,18 @@ package dev.alpas.fireplace.entities
 import dev.alpas.auth.Authenticatable
 import dev.alpas.auth.UserProvider
 import dev.alpas.md5
-import dev.alpas.ozone.MigratingTable
+import dev.alpas.ozone.OzoneEntity
+import dev.alpas.ozone.OzoneTable
 import dev.alpas.ozone.bigIncrements
 import me.liuwj.ktorm.dsl.eq
-import me.liuwj.ktorm.entity.Entity
 import me.liuwj.ktorm.entity.findById
 import me.liuwj.ktorm.entity.findOne
 import me.liuwj.ktorm.schema.timestamp
 import me.liuwj.ktorm.schema.varchar
 import java.time.Instant
 
-interface User : Entity<User>, Authenticatable {
-    companion object : Entity.Factory<User>()
+interface User : OzoneEntity<User>, Authenticatable {
+    companion object : OzoneEntity.Of<User>()
 
     override var id: Long
     override var email: String
@@ -36,13 +36,13 @@ interface User : Entity<User>, Authenticatable {
     }
 }
 
-object Users : MigratingTable<User>("users"), UserProvider {
-    val id by bigIncrements("id").bindTo { it.id }
+object Users : OzoneTable<User>("users"), UserProvider {
+    val id by bigIncrements()
     val email by varchar("email").index().unique().bindTo { it.email }
     val password by varchar("password").bindTo { it.password }
     val name by varchar("name").nullable().bindTo { it.name }
-    val createdAt by timestamp("created_at").nullable().bindTo { it.createdAt }
-    val updatedAt by timestamp("updated_at").nullable().bindTo { it.updatedAt }
+    val createdAt by createdAt()
+    val updatedAt by updatedAt()
     val emailVerifiedAt by timestamp("email_verified_at").nullable().bindTo { it.emailVerifiedAt }
 
     override fun findByUsername(username: Any): User? {
