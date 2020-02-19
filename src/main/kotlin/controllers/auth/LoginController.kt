3546f2c168ce79f428301d7c071e0a31cf43827e
vanessa gertman <vanessagertman@gmail.com>
2020-02-18 21:50:22 -0700

    Set the afterLoginRedirect url to /projects

--- src/main/kotlin/controllers/auth/LoginController.kt
+++ src/main/kotlin/controllers/auth/LoginController.kt
@@ -1,6 +1,11 @@
 package dev.alpas.fireplace.controllers.auth
 
 import dev.alpas.auth.HandlesUserLogin
+import dev.alpas.http.HttpCall
 import dev.alpas.routing.Controller
 
-class LoginController : Controller(), HandlesUserLogin
+class LoginController : Controller(), HandlesUserLogin {
+    override fun afterLoginRedirectTo(call: HttpCall): String {
+      return "/projects";
+    }
+}
