4adda9450a19485c7be73386973e239de1ef7f7a
vanessa gertman <vanessagertman@gmail.com>
2020-03-04 21:52:04 -0700

    Add validation rules for creating a project. Clean up ProjectController by moving logic into the new validation guard.

---
+++ src/main/kotlin/guards/CreateProjectGuard.kt
@@ -0,0 +1,30 @@
+package dev.alpas.fireplace.guards
+
+import dev.alpas.fireplace.entities.Project
+import dev.alpas.fireplace.entities.Projects
+import dev.alpas.ozone.create
+import dev.alpas.validation.Min
+import dev.alpas.validation.Required
+import dev.alpas.validation.ValidationGuard
+import dev.alpas.validation.Rule
+
+class CreateProjectGuard : ValidationGuard() {
+    override fun rules(): Map<String, Iterable<Rule>> {
+        // title required, not null or empty
+        // description not null or empty
+
+
+        return mapOf(
+            "title" to listOf(Required(), Min(8)),
+            "description" to listOf(Required())
+        )
+    }
+
+    fun commit(): Project {
+        return  Projects.create{
+            it.title to call.param("title")
+            it.description to call.param("description")
+            it.ownerId to call.user.id
+        }
+    }
+}
