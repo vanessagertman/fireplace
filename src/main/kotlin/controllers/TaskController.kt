7dd3fe63d97da0d02fe098d6cd260e8d829d14a3
vanessa gertman <vanessagertman@gmail.com>
2020-05-17 23:26:10 -0600

    Log activity when task is created or updated

--- src/main/kotlin/controllers/TaskController.kt
+++ src/main/kotlin/controllers/TaskController.kt
@@ -1,6 +1,6 @@
 package dev.alpas.fireplace.controllers
 
-import dev.alpas.fireplace.entities.Tasks
+import dev.alpas.fireplace.entities.*
 import dev.alpas.fireplace.guards.CreateTasksGuard
 import dev.alpas.fireplace.guards.UpdateTasksGuard
 import dev.alpas.http.HttpCall
@@ -8,11 +8,13 @@ import dev.alpas.orAbort
 import dev.alpas.routing.Controller
 import me.liuwj.ktorm.dsl.delete
 import me.liuwj.ktorm.dsl.eq
+import me.liuwj.ktorm.dsl.insert
 
 class TaskController : Controller() {
     fun store(call: HttpCall) {
        call.validateUsing(CreateTasksGuard::class) {
            val task = commit()
+           logTaskActivity(task, mapOf("action" to "created a task", "title" to task.body))
            call.replyAsJson(task)
        }
     }
@@ -26,7 +28,21 @@ class TaskController : Controller() {
     fun update(call: HttpCall) {
         call.validateUsing(UpdateTasksGuard::class) {
             val task = commit()
+            logTaskActivity(task, mapOf("action" to "updated a task", "title" to task.body))
             call.acknowledge()
+        }
+    }
+
+    private fun logTaskActivity(task: Task, payload: Map<String, Any?>) {
+        val now = call.nowInCurrentTimezone().toInstant()
+        val user = caller<User>()
+        Activities.insert {
+            it.payload to payload
+            it.projectId to task.project.id
+            it.tasId to task.id
+            it.userId to user.id
+            it.createdAt to now
+            it.updatedAt to now
         }
     }
 }
